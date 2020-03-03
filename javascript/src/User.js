4b06644c075f114a08d05435debfe450c4935282
fong <fong@meepshop.com>
2020-03-03 11:12:05 +0800

    Update js/User to have same behaviors as java version does

--- javascript/src/User.js
+++ javascript/src/User.js
@@ -1,5 +1,24 @@
-"use strict";
+'use strict';
 
 module.exports = class User {
+  constructor(trips, friends) {
+    this.trips = trips;
+    this.friends = friends;
+  }
 
-}
+  getFriends() {
+    return this.friends;
+  }
+
+  addFriend(user) {
+    this.friends.push(user);
+  }
+
+  addTrip(trip) {
+    this.trips.add(trip);
+  }
+
+  trips() {
+    return this.trips;
+  }
+};
