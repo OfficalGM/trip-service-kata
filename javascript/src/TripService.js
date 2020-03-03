ac6bc883813eda120756613d11bca299bdef1d51
fong <fong@meepshop.com>
2020-03-03 11:46:20 +0800

    Add tests coverage

--- javascript/src/TripService.js
+++ javascript/src/TripService.js
@@ -1,30 +1,38 @@
-"use strict";
+'use strict';
 
 let UserSession = require('./UserSession');
 let TripDAO = require('./TripDAO');
 
 class TripService {
-    getTripsByUser(user) {
-        let tripList = [];
-        let loggedUser = UserSession.getLoggedUser();
-        let isFriend = false;
-        if (loggedUser != null) {
-            let friends = user.getFriends();
-            for (let i=0; i < friends.length; i++) {
-                let friend = friends[i];
-                if (friend == loggedUser) {
-                    isFriend = true;
-                    break;
-                }
-            };
-            if (isFriend) {
-                tripList = TripDAO.findTripsByUser(user);
-            }
-            return tripList;
-        } else {
-            throw new Error('User not logged in.');
+  getTripsByUser(user) {
+    let tripList = [];
+    let loggedUser = this._getLoggedUser();
+    let isFriend = false;
+    if (loggedUser != null) {
+      let friends = user.getFriends();
+      for (let i = 0; i < friends.length; i++) {
+        let friend = friends[i];
+        if (friend == loggedUser) {
+          isFriend = true;
+          break;
         }
+      }
+      if (isFriend) {
+        tripList = this._findTripsByUser(user);
+      }
+      return tripList;
+    } else {
+      throw new Error('User not logged in.');
     }
+  }
+
+  _getLoggedUser() {
+    return UserSession.getLoggedUser();
+  }
+
+  _findTripsByUser() {
+    return TripDAO.findTripsByUser();
+  }
 }
 
-module.exports = TripService
+module.exports = TripService;
--- javascript/test/TripServiceSpec.js
+++ javascript/test/TripServiceSpec.js
@@ -1,12 +1,49 @@
-"use strict";
+'use strict';
 
 let assert = require('assert');
 let TripService = require('../src/TripService');
+const Trip = require('../src/Trip');
+const User = require('../src/User');
+const NOT_LOGGED_UESR = null;
+const LOGGED_USER = new User('John');
+let loggedUser = new User('John');
+let targetUser = new User('Mery');
+
+class TestTripService extends TripService {
+  _getLoggedUser() {
+    return loggedUser;
+  }
+  _findTripsByUser(user) {
+    return user.trips;
+  }
+}
+let tripService = new TestTripService();
 
 describe('TripService', () => {
+  it('should... ', () => {
+    assert.equal(2 + 2, 4);
+  });
 
-    it('should... ', () => {
-        assert.equal(2+2, 5);
+  it('should trow exception when user is not logged in', () => {
+    loggedUser = NOT_LOGGED_UESR;
+    assert.throws(() => {
+      tripService.getTripsByUser(targetUser);
     });
+  });
 
+  it('should not return trips when logged User is not a friend', () => {
+    loggedUser = LOGGED_USER; // without friends
+    const trips = tripService.getTripsByUser(targetUser);
+    assert.equal(trips.length, 0);
+  });
+
+  it('should return trips when logged User is a friend', () => {
+    loggedUser = LOGGED_USER; // without friends
+    const targetUser = new User('Mery', [new Trip(), new Trip()], [loggedUser]);
+    loggedUser.addFriend(targetUser);
+
+    const trips = tripService.getTripsByUser(targetUser);
+
+    assert.equal(trips.length, 2);
+  });
 });
