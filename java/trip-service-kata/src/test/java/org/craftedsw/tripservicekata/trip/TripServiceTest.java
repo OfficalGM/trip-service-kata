6442fc8414e79d29e4355f17ead9ab27c69ace5e
Tony <gm10285@gmail.com>
2020-09-30 22:41:20 +0800

    extract method

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -8,6 +8,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TripServiceTest {
 
+    User isLoggedUser = new User();
+
     @Test
     public void test_1() {
         TripService tripService = new TripService();
@@ -16,8 +18,30 @@ public class TripServiceTest {
         assertThatThrownBy(() -> {
             tripService.getTripsByUser(user);
 
-        }).isInstanceOf(CollaboratorCallException.class);
-
+        }).isInstanceOf(CollaboratorCallException.class).hasMessage("UserSession.getLoggedUser() should not be called in an unit test");
     }
+
+    @Test
+    public void test_2() {
+        TripService tripService = new FakeTripService();
+
+        User user = new User();
+        user.addFriend(isLoggedUser);
+
+        assertThatThrownBy(() -> {
+            tripService.getTripsByUser(user);
+
+        }).isInstanceOf(CollaboratorCallException.class).hasMessage("TripDAO should not be invoked on an unit test.");
+    }
+
+
+    class FakeTripService extends TripService {
+
+        @Override
+        User getLoggedUser() {
+            return isLoggedUser;
+        }
+    }
+
 
 }
