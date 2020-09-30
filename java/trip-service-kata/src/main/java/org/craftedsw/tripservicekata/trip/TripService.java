c35265689732eb7cce575921ad3a451decd4e7e8
Tony <gm10285@gmail.com>
2020-09-30 23:25:26 +0800

    add if remove if vaildate

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,29 +1,22 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
+
+import java.util.Collections;
+import java.util.List;
 
 public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
         User loggedUser = getLoggedUser();
-        validate(loggedUser);
-        return user.isFriendWith(loggedUser) ? findTripsByUser(user) : Collections.emptyList();
-    }
-
-
-    void validate(User loggedUser) {
         if (loggedUser == null) {
             throw new UserNotLoggedInException();
         }
+        return user.isFriendWith(loggedUser) ? findTripsByUser(user) : Collections.emptyList();
     }
-
+    
     List<Trip> findTripsByUser(User user) {
         return TripDAO.findTripsByUser(user);
     }
